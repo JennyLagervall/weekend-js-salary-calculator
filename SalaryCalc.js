36f83b15ba509bce3554de627849f174a3398f1d
Jenny Lagervall <jennylagervall@gmail.com>
2024-08-30 16:06:59 -0500

    form is grabbing values and adding to the table

--- SalaryCalc.js
+++ SalaryCalc.js
@@ -1,8 +1,40 @@
 console.log('hello!')
 
 function grabValue(event){
+    event.preventDefault();
+    const firstNameInput = document.getElementById('first-name-input').value;
+    const lastNameInput = document.getElementById("last-name-input").value;
+    const idInput = document.getElementById('id-input').value;
+    const titleInput = document.getElementById('title-input').value;
+    const salaryInput = document.getElementById('salary-input').value;
     
+    const tradd = document.getElementById("table-add");
+    const tdata = document.createElement('tr');
+    tradd.appendChild(tdata);
+    
+   tdata.innerHTML = 
+   `  <td>${firstNameInput}</td>
+      <td>${lastNameInput}</td>
+      <td>${idInput}</td>
+      <td>${titleInput}</td>
+      <td>${salaryInput}</td> `
+      
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function deleteValue(event){
 
