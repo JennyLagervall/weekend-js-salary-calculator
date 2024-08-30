319ea280679644aaf5f4ab4459f6f33e151048dd
Jenny Lagervall <jennylagervall@gmail.com>
2024-08-30 15:39:15 -0500

    html set up with form w id's and a table with a header

--- SalaryCalc.js
+++ SalaryCalc.js
@@ -3,3 +3,23 @@ console.log('hello!')
 function grabValue(event){
     
 }
+
+function deleteValue(event){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <button id="submit-button" onClick="deleteValue(event)">Delete</button></th>
