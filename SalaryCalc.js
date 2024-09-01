63bb968eba40f7f7fdcee6ac7678b9a411f6c629
Jenny Lagervall <jennylagervall@gmail.com>
2024-09-01 17:40:29 -0500

    added annual salary input to array, loop array to get total monthly/12

--- SalaryCalc.js
+++ SalaryCalc.js
@@ -1,4 +1,5 @@
 console.log('hello!')
+const monthlySalary = [];
 
 function grabValue(event){
     event.preventDefault();
@@ -6,8 +7,23 @@ function grabValue(event){
     const lastNameInput = document.getElementById("last-name-input").value;
     const idInput = document.getElementById('id-input').value;
     const titleInput = document.getElementById('title-input').value;
-    const salaryInput = document.getElementById('salary-input').value;
+    const salaryInput = Number(document.getElementById('salary-input').value);
     
+    monthlySalary.push(salaryInput);
+
+    let salSum = 0
+    let months = 12
+    for (let i =0; i<monthlySalary.length; i++){
+       salSum += monthlySalary[i];}
+
+
+       let monthSum = salSum/ months;
+
+    console.log(monthSum);
+
+
+
+
     const tradd = document.getElementById("table-add");
     const tabledata = document.createElement('tr');
     tabledata.classList.add('newTr');
@@ -21,12 +37,40 @@ function grabValue(event){
       <td>${salaryInput}</td> 
       <td><button onClick="deleteValue(event)">delete</button></td>
       `
-      
 }
+  
 
 
-// function salaryCal (){
+
+    
+    
+
+
+
+
+    // const salaryObject = {
+    //    salary: salaryOutput,
+    // }
+    // monthlySalary.push(salaryObject);
+    // console.log('Updated monthlySalary:', monthlySalary);
+    
+    // }  
+    // const hThreeAdd = document.getElementById("hThree");
+    // const hThreeData = document.createElement('div');
+    // hThreeAdd.appendChild(hThreeData);
+    
+    // hThreeData.innerHTML = 
+    // `<p>${monthlySalary}</p>`
+
+//   let sum = 0
+//   for (let i =0; i < monthlySalary.length; i++);
+//   let sumOne = sum += monthlySalary[i];
+//   let monthlySum = sumOne / 12;
+//   monthlySum
+
+// }
  
+
     
 
 //     const footAppend = document.getElementById("table-add");
